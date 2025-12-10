4fa96aab259c65e0c5ed37b50a5d282bf6d868a0
jayppatell1996-lgtm <jay.p.patell1996@gmail.com>
2025-12-09 21:52:42 -0700

    Update drizzle.config.ts

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,8 @@ import type { Config } from 'drizzle-kit';
 export default {
   schema: './src/db/schema.ts',
   out: './drizzle',
-  dialect: 'turso',
+  dialect: 'sqlite',
+  driver: 'turso',
   dbCredentials: {
     url: process.env.TURSO_DATABASE_URL!,
     authToken: process.env.TURSO_AUTH_TOKEN,
